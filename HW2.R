aaecbd81c450353aa2856272124b93ee1b1860c9
lindgrenrebecca <rlindgren120@gmail.com>
2025-01-30 15:45:58 -0500

    Create HW2.R

---
+++ HW2.R
@@ -0,0 +1,15 @@
+#as written in Canvas
+
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Berine")
+ABC political poll results <- c(4, 62 51, 21, 2, 14, 15)
+CBS political poll results <- c(12, 75, 43, 19, 1, 21, 19) 
+
+
+
+#rewrite
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Berine")
+ABC_political_poll_results <- c(4, 62, 51, 21, 2, 14, 15)
+CBS_political_poll_results <- c(12, 75, 43, 19, 1, 21, 19) 
+
+df <- data.frame(Name, ABC_political_poll_results, CBS_political_poll_results)
+df
