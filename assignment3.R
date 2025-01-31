4d8b46e7633071d1faaee90a62fe63c0ed7a74db
lindgrenrebecca <rlindgren120@gmail.com>
2025-01-31 16:54:27 -0500

    Update assignment3.R

--- assignment3.R
+++ assignment3.R
@@ -1,5 +1,4 @@
 #as written in Canvas
-
 Name <- c("Jeb", "Donald", "Ted", "Marco" "Carly", "Hillary", "Berine")
 ABC political poll results <- c(4, 62 51, 21, 2, 14, 15)
 CBS political poll results <- c(12, 75, 43, 19, 1, 21, 19) 
