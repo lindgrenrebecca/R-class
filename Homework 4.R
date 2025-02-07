8796aa2de3e7010e00dc3b2dc286a941082d0246
lindgrenrebecca <rlindgren120@gmail.com>
2025-02-07 10:36:31 -0500

    Update Homework 4.R

--- Homework 4.R
+++ Homework 4.R
@@ -1,14 +1,20 @@
+#Write vectors
 Freq <- c(.6, .3, .4, .4, .2, .6, .3, .4, .9, .2)
 bloodp <- c(103, 87, 32, 42, 59, 109, 78, 205, 135, 176)
 first <- c("bad", "bad", "bad", "bad", "good", "good", "good", "good", NA, "bad")
 second <- c("low", "low", "high", "high","low", "low","high", "high","high", "high")
 finaldecision <- c("low", "high", "low", "high", "low", "high", "low", "high", "high", "high")
 
+#Switch words to binary variable
 first <- ifelse(first == "bad", 1, 0)
 second <- ifelse(second == "low", 0, 1)
 finaldecision <- ifelse(finaldecision == "low", 0, 1)
 
-df <- data.frame(Freq, bloodp, first, second, finaldecision)
-
-hist(df$bloodp)
-boxplot(df$bloodp)
+#Set up a 2x2 plot space for four plots
+par(mfrow = c(2, 2))
+#Histograms
+hist(bloodp, main = "Blood Pressure")
+hist(finaldecision, breaks = c(-0.5, 0.5, 1.5), xlim = c(-0.5, 1.5), main = "Decision of Treatment")
+#Boxplots
+boxplot(bloodp, horizontal = T, main = "Blood Pressure")
+boxplot(finaldecision, horizontal = T, main = "Decision of Treatment")
