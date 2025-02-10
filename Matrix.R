b310c5fce2cf61a04a88c760ff803f45955e91c3
lindgrenrebecca <rlindgren120@gmail.com>
2025-02-10 14:01:00 -0500

    Matrix Homework

---
+++ Matrix.R
@@ -0,0 +1,18 @@
+#A 10x10 matrix with 1:100
+A = matrix(1:100, nrow = 10)
+#A 10x100 matrix with 1:1000
+B = matrix(1:1000, nrow = 10)
+
+##Assignment: Find the value of inverse of a matrix, determinant of a matrix##
+
+#finding the inverse of matrix A
+solve(A)
+
+#finding the inverse of matrix B
+solve(B)
+
+#finding the determinant of matrix A
+det(A)
+
+#finding the determinant of matrix A
+det(B)
